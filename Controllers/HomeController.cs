b3c4c32720e6d6ef5f957103a58ae1a74d27ef69
joshua.m.willis8 <joshua.m.willis8@gmail.com>
2021-11-26 14:13:06 -0800

    Removed Math.Floor in favor of implicit integer arithmetic (brain fart) Added project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2_Denominations.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+
+		public ActionResult Index(string id, int amount)
+		{
+			List<int> acceptedDenominations = new List<int> { 1000, 500, 100, 10, 1 };
+			ViewBag.Denominations = new Dictionary<int, int>();
+
+			int remainder = amount;
+
+			foreach (int acceptedDenomination in acceptedDenominations)
+			{
+				if (remainder >= acceptedDenomination)
+				{
+					ViewBag.Denominations[acceptedDenomination] = remainder / acceptedDenomination;
+					remainder %= acceptedDenomination;
+				}
+			}
+			return View();
+		}
+	}
+}
